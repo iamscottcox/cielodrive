d7c6547733b34541f744538764f27e91726ad119
Scott Cox <scottcox89@gmail.com>
2019-03-19 21:06:10 +0000

    Add <Logo/> component

---
+++ src/components/Logo/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import CieloDriveLogoPNG from "../../images/logos/CieloDriveLogo.png";
+
+export const Logo = ({ className }) => (
+  <img
+    className={className}
+    src={CieloDriveLogoPNG}
+    alt="The Cielo Drive heart logo"
+  />
+);
