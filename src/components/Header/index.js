bf846b077197654aac303841f6a54a2115e901b6
Scott Cox <scottcox89@gmail.com>
2019-05-09 21:02:49 +0100

    Add styles to <Header/>

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,24 +3,34 @@ import styled from "styled-components";
 
 export const StyledHeader = styled.header`
   position: fixed;
-  display: flex;
-  justify-content: space-around;
   width: 100%;
   padding: 1em;
   background: rgba(255, 255, 255, 0.5);
-
-  a {
-    color: #333;
-    text-decoration: none;
-    font-family: "Staatliches", cursive;
+  z-index: 100;
+  
+  ul {
+    display: flex;
+    justify-content: space-around;
+    
+    li {
+      list-style: none;
+    
+      a {
+        color: #333;
+        text-decoration: none;
+        font-family: "Staatliches", cursive;
+      } 
+    }
   }
 `;
 
 export const Header = () => (
   <StyledHeader>
-    <a href="https://www.facebook.com/cielodriveuk">Facebook</a>
-    <a href="https://www.instagram.com/cielodrive_uk">Instagram</a>
-    <a href="https://twitter.com/CIELODRIVEUK">Twitter</a>
+    <ul>
+      <li><a href="https://www.facebook.com/cielodriveuk">Facebook</a></li>
+      <li><a href="https://www.instagram.com/cielodrive_uk">Instagram</a></li>
+      <li><a href="https://twitter.com/CIELODRIVEUK">Twitter</a></li>
+    </ul>
   </StyledHeader>
 );
 
