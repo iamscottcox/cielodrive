8695d338cf4ae783ad87bd7cb690271febde79cc
Scott Cox <scottcox89@gmail.com>
2019-05-09 20:35:26 +0100

    Remove box-shadow from <Header/> styles

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -8,7 +8,6 @@ export const StyledHeader = styled.header`
   width: 100%;
   padding: 1em;
   background: rgba(255, 255, 255, 0.5);
-  box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
 
   a {
     color: #333;
